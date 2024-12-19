95bbfe77b7aae0999ce0fb46c1753d47cc4afc70
Alex__Zubenko <alex.zubenko@tapslots.com>
2024-12-19 14:20:09 +0200

    create boolet

--- Assets/SO/Scripts/GameSoInstaller.cs
+++ Assets/SO/Scripts/GameSoInstaller.cs
@@ -9,11 +9,13 @@ namespace SO.Scripts
     {
         [SerializeField] public CharacterConfig _characterConfig;
         [SerializeField] public EnvironmentConfig _environmentConfig;
+        [SerializeField] public BooletConfig _booletConfig;
 
         public override void InstallBindings()
         {
             Container.BindInstance(_characterConfig).IfNotBound();
             Container.BindInstance(_environmentConfig).IfNotBound();
+            Container.BindInstance(_booletConfig).IfNotBound();
         }
     }
 }
